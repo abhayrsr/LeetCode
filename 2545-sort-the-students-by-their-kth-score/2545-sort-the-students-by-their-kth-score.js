ccaeffc0fdb985adb2c4cbb0f9b3517271288b33
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2023-06-13 23:45:37 +0530

    Time: 156 ms (6.05%), Space: 51.2 MB (75.80%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.js
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.js
@@ -4,13 +4,31 @@
  * @return {number[][]}
  */
 var sortTheStudents = function(score, k) {
-   for(let i = 0; i < score.length; i++){
-       for(let j = 0; j < score.length; j++){
-           if(score[i][k] > score[j][k]){
-               [score[i], score[j]] = [score[j], score[i]];
-           }
-       }
-   }
+    let extra = new Map();
+    
+    for(let i = 0; i < score.length; i++){
+        extra.set(score[i][k], i);
+    }
+    
+    let sorted = ([...extra.entries()].sort(([keyA],[keyB]) => keyB - keyA));
+    let sortedMap = new Map(sorted);
+    
+    let ans = [];
+    
+    for(const value of sortedMap.values()){
+        ans.push(score[value]);
+    }
+    
+    return ans;
+   
 
-   return score;
+//    for(let i = 0; i < score.length; i++){
+//        for(let j = 0; j < score.length; j++){
+//            if(score[i][k] > score[j][k]){
+//                [score[i], score[j]] = [score[j], score[i]];
+//            }
+//        }
+//    }
+
+//    return score;
 };
