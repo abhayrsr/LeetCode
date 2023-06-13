95be4150ee6a447d9f9e142b040f839bc39e71b0
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2023-06-13 13:30:26 +0530

    Time: 133 ms (41.64%), Space: 51.6 MB (34.16%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} score
+ * @param {number} k
+ * @return {number[][]}
+ */
+var sortTheStudents = function(score, k) {
+   for(let i = 0; i < score.length; i++){
+       for(let j = 0; j < score.length; j++){
+           if(score[i][k] > score[j][k]){
+               [score[i], score[j]] = [score[j], score[i]];
+           }
+       }
+   }
+
+   return score;
+};
