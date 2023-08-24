8a9cc976cab5f8b80880786b2a6c09a3e3609af0
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2023-08-24 19:43:27 +0530

    Time: 260 ms (97.47%), Space: 81 MB (79.75%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.js
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.js
@@ -4,20 +4,17 @@
  * @return {number[]}
  */
 var getAverages = function(nums, k) {
-    if(k > nums.length){
-        return Array(nums.length).fill(-1);
-    }
+   if(k > nums.length){
+       return Array(nums.length).fill(-1);
+   }
     const result = Array(nums.length).fill(-1);
-    console.log(result);
-    let left = 0;
     let sum = 0;
-    
-    for(right= 0; right < nums.length; right++){
-        sum+=nums[right]
-        
-        if(right - left + 1 === k*2 + 1){
-            const avg = Math.floor(sum/(k*2 + 1));
-            result[right-k] = avg;
+    let left = 0;
+    for(let right = 0; right < nums.length; right++){
+        sum+=nums[right];
+        if(right - left + 1 === 2*k + 1){
+            let avg = Math.floor(sum/(2*k+1));
+            result[right - k] = avg;
             sum-=nums[left];
             left++;
         }
