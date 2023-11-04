b3b5ae1ac5055753c47e9578884c458b2a2eec1f
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2023-11-04 22:48:14 +0530

    Time: 56 ms (100.00%), Space: 47 MB (40.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2211-count-collisions-on-a-road/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2211-count-collisions-on-a-road.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} directions
+ * @return {number}
+ */
+var countCollisions = function(directions) {
+    let left = 0, right = directions.length - 1;
+    
+    while(left < directions.length && directions.charAt(left) == "L"){
+        left++;
+    }
+    
+    while(right >= 0  && directions.charAt(right) == "R"){
+        right--;
+    }
+    
+    let count = 0;
+    
+    for(let i = left; i <= right; i++){
+        if(directions[i] !== "S"){
+            count++;
+        }
+    }
+    
+    return count;
+    
+};
