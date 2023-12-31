b5aab2d00676e878e83153547f58d0a199c6aded
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2023-12-31 16:38:30 +0530

    Time: 42 ms (96.67%), Space: 42.2 MB (74.44%) - LeetHub

---
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxLengthBetweenEqualCharacters = function(s) {
+    let map = new Map();
+    let ans = -1;
+
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s[i])) {
+            ans = Math.max(ans, i - map.get(s[i]) - 1);
+        } else {
+            map.set(s[i], i);
+        }
+    }
+
+    return ans;
+   
+    
+};
