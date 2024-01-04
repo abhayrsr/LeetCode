eb93153711158ba7f58c1ad7a71ccc653258a46e
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-01-04 21:35:57 +0530

    Time: 122 ms (53.85%), Space: 56.3 MB (28.21%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function(nums) {
+    let count = 0;
+    let map = new Map();
+    
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(nums[i])){
+            map.set(nums[i], map.get(nums[i]) + 1);
+        } else {
+            map.set(nums[i], 1)
+        }
+    }
+    
+    for(let [key, values] of map.entries()){
+        
+        if(map.get(key) == 1){
+            return -1;
+        }
+        else if(map.get(key) % 3 == 0){
+            count += parseInt(map.get(key)/3);
+        }
+        else if(map.get(key) % 3 == 1){
+            count += (parseInt(map.get(key)/3) - 1) + 2;
+        }
+        else if(map.get(key) % 3 == 2){
+            count += parseInt(map.get(key)/3) + 1;
+        }
+        // console.log(count);
+    }
+    return count;
+};
