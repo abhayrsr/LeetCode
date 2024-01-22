279f8520bc25101cdb3f75077c563e11a8990c65
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-01-22 15:18:16 +0530

    Time: 51 ms (93.51%), Space: 50 MB (5.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,23 +3,44 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    let verify = parseInt(nums.length / 3);
-    nums.sort((a, b) => a - b);
-    let count = 0, track = nums[0];
-    let result = new Set();
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == track) {
-            count++;
-        } else {
-            count = 1;
-            track = nums[i];
+    let verify = parseInt(nums.length/3);
+    let count1 = 0, ele1 = -Infinity;
+    let count2 = 0, ele2 = -Infinity;
+    let res = [];
+    
+    for(let i = 0; i < nums.length; i++){
+        if(count1 == 0 && nums[i] !== ele2){
+            count1 = 1;
+            ele1 = nums[i];
         }
-
-        if (count > verify && !result.has(nums[i])) {
-            result.add(nums[i]);
+        else if(count2 == 0 && nums[i] !== ele1){
+            count2 = 1;
+            ele2 = nums[i];
+        }
+        else if(ele1 == nums[i]){
+            count1++;
+        }
+        else if(ele2 == nums[i]){
+            count2++;
+        }
+        else{
+            count1--;
+            count2--;
         }
     }
-
-    return Array.from(result); 
+    
+    count1 = 0; 
+    count2 = 0;
+    
+    for(let i = 0; i < nums.length; i++){
+        if(ele1 == nums[i]) count1++;
+        else if(ele2 == nums[i]) count2++;
+    }
+    if(count1 > verify){
+        res.push(ele1);
+    }
+    if(count2 > verify){
+        res.push(ele2);
+    }
+    return res;
 };
