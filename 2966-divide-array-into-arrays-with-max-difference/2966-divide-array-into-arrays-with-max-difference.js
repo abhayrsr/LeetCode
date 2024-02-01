92d1676e0e15331d3858aa3aa2ae3a733ce9d773
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-02-01 22:56:14 +0530

    Time: 347 ms (37.14%), Space: 78.7 MB (98.10%) - LeetHub

---
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
+var divideArray = function(nums, k) {
+     nums.sort((a, b) => a - b)
+    const res = []
+    for (i = 2; i < nums.length; i = i + 3) {
+        if ((nums[i - 1] - nums[i - 2] > k) || (nums[i] - nums[i - 1] > k) || ((nums[i] - nums[i - 2] > k))) {
+            return []
+        } else {
+            res.push([nums[i - 2], nums[i - 1], nums[i]])
+        }
+    }
+    return res
+};
