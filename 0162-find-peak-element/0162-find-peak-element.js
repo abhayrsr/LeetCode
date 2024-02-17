36c8166c0d009a513b65912589dff719e0f8d168
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-02-17 12:58:31 +0530

    Time: 52 ms (58.76%), Space: 49.1 MB (18.26%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    if(nums.length == 1) return 0;
+    if(nums[0] > nums[1]) return 0;
+    if(nums[nums.length-1] > nums[nums.length-2]) return nums.length-1;
+    
+    let low = 1, high = nums.length - 2;
+    
+    while(low <= high){
+        let mid = parseInt((low+high)/2);
+        
+        if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+            return mid;
+        
+        else if(nums[mid] > nums[mid-1])
+            low = mid+1;
+        
+        else if(nums[mid] < nums[mid-1])
+            high = mid-1;
+    }
+};
