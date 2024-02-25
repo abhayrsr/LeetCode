8a8f08ddfa1b76183b7e9badafaeb416c4eb035b
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-02-25 15:36:00 +0530

    Time: 310 ms (53.86%), Space: 52.6 MB (24.84%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let row = 0, col = matrix[0].length - 1;
+    while(row < matrix.length && col >= 0){
+        if(matrix[row][col] == target) return true;
+        else if(matrix[row][col] < target){
+            row++;
+        } else {
+            col--;
+        }
+    }
+    return false;
+};
