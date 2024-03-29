bee352795a65da42a8895c2f2ba8ac9e8acc6821
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-03-29 12:51:23 +0530

    Time: 191 ms (31.03%), Space: 59.6 MB (13.99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -9,11 +9,12 @@ var longestPalindrome = function(s) {
     for(let i = 0; i < s.length; i++){
         let l = i;
         let r = i;
-        
+        //odd length
         while(l >= 0 && r < s.length && s[l] == s[r]){
             if(r-l+1 > resLen){
                 res = s.substring(l, r+1);
                 resLen = r-l+1;
+                console.log(res);
             }
             l-=1;
             r+=1;
@@ -21,11 +22,12 @@ var longestPalindrome = function(s) {
         
         l = i;
         r = i+1;
-        
+        //even length
         while(l >= 0 && r < s.length && s[l] == s[r]){
             if(r-l+1 > resLen){
                 res = s.substring(l, r+1);
                 resLen = r-l+1;
+                console.log(res, "n");
             }
             l-=1;
             r+=1;
