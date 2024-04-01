a327fab6280a38a97113e7442397a5b43a072faa
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-04-01 10:01:45 +0530

    Time: 63 ms (55.58%), Space: 49.8 MB (89.94%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -4,10 +4,8 @@
  */
 var singleNumber = function(nums) {
     let count = 0;
-    
     for(let i = 0; i < nums.length; i++){
-        count = count ^ nums[i];
+        count = count^nums[i];
     }
-    
     return count;
 };
