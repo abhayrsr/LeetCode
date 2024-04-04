3dc88e12cb62fe37b8996d0f65e8b5f1712fb585
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-04-04 20:40:12 +0530

    Time: 47 ms (95.46%), Space: 49.9 MB (71.50%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+      let temp = [];
+    for(let i = 0; i < s.length; i++){
+        let curr = s[i];
+        let tempLast = temp[temp.length - 1];
+        
+        if(curr == '(') temp.push(')');
+        else if(curr == '{') temp.push('}');
+        else if(curr == '[') temp.push(']');
+        else if(curr == tempLast) temp.pop();
+        else return false;
+    }
+    
+    return temp.length === 0;
+    
+};
