723322901d2722441e638ee3b53c1cb055d7277b
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-07-14 23:38:55 +0530

    Time: 65 ms (43.83%), Space: 51.3 MB (58.82%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,22 +3,11 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    if(nums.length == 1){
-        if(nums[0] == 0) return 1;
-        else if(nums[0] == 1) return 0;
-    };
-    let n = nums.length;
-    let asum = n*(n+1)/2;
-    let sum = 0;
-    
-    for(let i = 0; i < nums.length; i++){
-        sum = sum + nums[i];
+    let sum1 = 0,  sum2 = 0
+    for(let i=0; i < nums.length; i++){
+        sum1+=nums[i]
+        sum2+=i
     }
-    
-    if(asum == sum && nums[0] == 0){
-        return nums[n-1] + 1;
-    } else {
-        return asum-sum;
-    }
-    
+    sum2 = sum2 + nums.length;
+    return sum2 - sum1;
 };
