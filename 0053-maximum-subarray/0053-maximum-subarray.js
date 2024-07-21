9b24cea7a2fe1a83d5a53ddc7877a19300848152
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-07-21 22:34:30 +0530

    Time: 75 ms (43.96%), Space: 57.6 MB (49.20%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let max = -Infinity, sum = 0, track = max;
-    for(let i = 0; i < nums.length; i++){
-        sum += nums[i];
+    let max = -Infinity, sum = 0;
+    for(let i=0; i < nums.length; i++){
+        sum += nums[i]
         max = Math.max(max, sum);
         if(sum < 0){
             sum = 0;
         }
     }
+    
     return max;
 };
