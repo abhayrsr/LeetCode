443b84520da13efe3d142da0923c1983f3a7508b
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-07-22 23:43:33 +0530

    Time: 80 ms (32.06%), Space: 59.4 MB (29.67%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let min = prices[0], track = min, res = 0;
+     let min = prices[0], track = min, res = 0;
     for(let i = 1; i < prices.length; i++){
         min = Math.min(min, prices[i]);
         res = Math.max(res, prices[i] - min);
