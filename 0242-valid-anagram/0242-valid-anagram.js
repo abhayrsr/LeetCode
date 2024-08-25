0f77866c706c400a5137dd157b0bcd22d4ee39aa
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-08-25 20:29:30 +0530

    Time: 73 ms (48.91%), Space: 52.2 MB (63.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,22 +3,24 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    let arr = new Array(26).fill(0);
-    
-    if(s.length !== t.length) return false;
-    
-    for(let i = 0; i < s.length; i++){
-        arr[s.charCodeAt(i) - 'a'.charCodeAt(0)] += 1;
+var isAnagram = function(s, t) {   
+    if (s.length !== t.length) {
+        return false;
     }
-    
-    for(let i = 0; i < t.length; i++){
-        if(arr[t.charCodeAt(i) - 'a'.charCodeAt(0)]){
-            arr[t.charCodeAt(i) - 'a'.charCodeAt(0)] -= 1;
-        } else {
+
+    const counter = new Map();
+
+    for (let char of s) {
+        counter.set(char, (counter.get(char) || 0) + 1);
+    }
+
+    for (let char of t) {
+        if (!counter.has(char) || counter.get(char) === 0) {
             return false;
         }
-        
+        counter.set(char, counter.get(char) - 1);
     }
-    return true; 
+
+    return true;    
+    
 };
