83a869d430be8381ec92144923e6ebcf10e6bc98
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-08-29 00:11:21 +0530

    Time: 71 ms (50.18%), Space: 52.8 MB (80.40%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,37 +4,23 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let count = new Map();
-    let bucket = new Array(nums.length + 1);
+    let map = new Map();
     
-    for(let n in nums){
-        if(count.has(nums[n])){
-            count.set(nums[n], count.get(nums[n]) + 1);
+    for(let n = 0; n < nums.length; n++){
+        if(map.has(nums[n])){
+            map.set(nums[n], map.get(nums[n]) + 1)
         } else {
-            count.set(nums[n], 1)
+            map.set(nums[n], 1)
         }
     }
     
-     count.forEach((freq, key) => {
-        if (!bucket[freq]) {
-            bucket[freq] = [];
-        }
-        bucket[freq].push(key);
-    });
+    let sorted = Array.from(map.entries()).sort((a, b) => b[1] - a[1]);
     
-    // console.log(bucket)
+    let res = []
     
-    const res = [];
-    
-    for (let i = bucket.length - 1; i >= 0 && k > 0; i--) {
-        if (bucket[i]) {
-            for (const ele of bucket[i]) {
-                res.push(ele);
-                k--;
-            }
-        }
+    for(let i = 0; i < k; i++){
+        res.push(sorted[i][0])
     }
-
+    
     return res;
-    
 };
