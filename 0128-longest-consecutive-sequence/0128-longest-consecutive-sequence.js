ca3c16e4b2df45cba0f386b55b2ceb408cb921e5
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-09-16 00:41:48 +0530

    Time: 1240 ms (16.01%), Space: 72.7 MB (61.80%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -8,38 +8,17 @@ var longestConsecutive = function(nums) {
     let set = new Set(nums)
     let longestStreak = 0
     
-    for(let num of set){
-        if(!set.has(num-1)){
-            let currentNum = num
-            let currentStreak = 1
-            
-            while(set.has(currentNum+1)){
-                currentNum++
-                currentStreak++
-            }
-            longestStreak = Math.max(currentStreak, longestStreak)
-        }
+    for(let num of nums){
+        let currentNum = num
+        let currentStreak = 0
         
+        if(!set.has(num-1)){
+                while(set.has(currentNum)){
+                    currentNum++
+                    currentStreak++
+                }
+            }
+        longestStreak = Math.max(currentStreak, longestStreak)
     }
     return longestStreak
-//     let numSet = new Set(nums);
-//     let longestStreak = 0;
-//     console.log(numSet)
-
-//     for (let num of numSet) {
-//         console.log(num)
-//         if (!numSet.has(num - 1)) {
-//             let currentNum = num;
-//             let currentStreak = 1;
-
-//             while (numSet.has(currentNum + 1)) {
-//                 currentNum++;
-//                 currentStreak++;
-//             }
-
-//             longestStreak = Math.max(longestStreak, currentStreak);
-//         }
-//     }
-
-//     return longestStreak;
 };
