c494c068e7440122646408f6610c0e4b1062d65d
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-09-25 22:28:57 +0530

    Time: 65 ms (75.58%), Space: 49.9 MB (93.85%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let left = 0, right = 0, max = 0;
+    let visited = {}
+    
+    while(right < s.length){
+        const char = s.charAt(right);
+        visited[char] = visited[char] ? visited[char] + 1 : 1;
+        
+        if(visited[char] > max) max = visited[char];
+        
+        while((right - left + 1) - max > k){
+            visited[s.charAt(left)]--;
+            left++;
+        }
+        
+        right++
+    }
+    
+    return right - left;
+    
+};
