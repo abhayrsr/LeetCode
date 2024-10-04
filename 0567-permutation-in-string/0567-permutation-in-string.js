48d560567800a3758ddc2447673d192f3d985171
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-10-04 11:21:06 +0530

    Time: 62 ms (89.27%), Space: 53.9 MB (49.49%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -9,24 +9,27 @@ var checkInclusion = function (s1, s2) {
   let right = 0;
 
   while (left <= right && right < s2.length) {
-    const charRight = s2[right];
-    if (map.get(charRight) > 0) {
-      map.set(charRight, map.get(charRight) - 1);
-      right++;
-    } else if (!map.has(charRight)) {
-      while (left != right) {
-        const charLeft = s2[left++];
-        if (map.has(charLeft)) map.set(charLeft, map.get(charLeft) + 1);
+      const charRight = s2.charAt(right)
+      if(map.get(charRight) > 0){
+          map.set(charRight, map.get(charRight) - 1)
+          right++
+      } else if(!map.has(charRight)){
+          while(left != right){
+              const charLeft = s2[left++];
+              if (map.has(charLeft)) map.set(charLeft, map.get(charLeft) + 1);
+              }
+              left++;
+              right++;
+          
+      }  else {
+          const charLeft = s2.charAt(left)
+          
+          map.set(charLeft, map.get(charLeft) + 1)
+          left++
       }
-      left++;
-      right++;
-    } else {
-      const charLeft = s2[left++];
-        console.log(charLeft)
-      map.set(charLeft, map.get(charLeft) + 1);
-    }
-
-    if (right - left >= s1.length) return true;
+      
+      if (right - left >= s1.length) return true;
+      
   }
   return false;
 };
