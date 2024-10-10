03de7cad017a3a32c136293a24818cf4375ce724
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-10-10 18:14:34 +0530

    Time: 57 ms (29.42%), Space: 49.3 MB (20.56%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,27 +4,35 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let ans = -1;
-    function find(arr, ele){
-        let low = 0, high = arr.length-1;
-        while(low <= high){
-            let mid = parseInt((low + high)/2);
-            if(arr[mid] > ele){
-                high = mid - 1;
+    let row = matrix.length, col = matrix[0].length
+    let top = 0, bot = row - 1
+    let rowTarget = 0;
+    
+    while(top <= bot){
+        rowTarget = parseInt((top + bot) / 2)
+        
+        if(target > matrix[rowTarget][col - 1]){
+            top = rowTarget + 1
+        } else if(target < matrix[rowTarget][0]){
+            bot = rowTarget - 1
+        } else {
+            break;
+        }
+    }
+    
+    if(top > bot) return false
+    else {
+        let l = 0, r = col - 1
+        while(l <= r){
+            let m = parseInt((l+r)/2)
+            if(target > matrix[rowTarget][m]){
+                l = m + 1
+            } else if(target < matrix[rowTarget][m]){
+                r = m - 1
             } else {
-                ans = arr[mid];
-                low = mid + 1;
+                return true
             }
         }
-        
-        if(ans == target) return true;
-        else return false;
+        return false;
     }
-    for(let i = 0; i < matrix.length; i++){
-        let bool = find(matrix[i], target);
-        if(bool == true){
-            return true;
-        } 
-    }
-    return false;
 };
