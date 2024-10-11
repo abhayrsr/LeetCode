ecac2a4fd0ee02c5a1d67beed496d3087b4c17b9
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-10-11 11:35:09 +0530

    Time: 62 ms (80.40%), Space: 52.4 MB (58.92%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.js
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -4,28 +4,29 @@
  * @return {number}
  */
 var minEatingSpeed = function(piles, h) {
-    let low = 1, high = Math.max(...piles);
-    let ans = -1;
+    let result = -Infinity;
     
-    function totalHours(piles, mid){
-        let hrs = 0;
-        for(let i = 0; i < piles.length; i++){
-            hrs += Math.ceil(piles[i]/mid); 
-        }
-        return hrs;
-    }
+   function totalHours(piles, mid){
+       let hrs = 0;
+       for(let i = 0; i < piles.length; i++){
+           hrs += Math.ceil(piles[i]/mid)
+       }
+       return hrs;
+   }
+    let low = 1, high = Math.max(...piles);
     
     while(low <= high){
-        let mid = parseInt((low+high)/2); 
-        let tH = totalHours(piles, mid);
+        let mid = parseInt((low + high)/ 2);
+        let tH = totalHours(piles, mid)
         
         if(tH <= h){
-            ans = tH;
-            high = mid-1;
+            result = mid
+            high = mid - 1
+            
         } else {
             low = mid + 1;
         }
     }
     
-    return low;
+    return result;
 };
