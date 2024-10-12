f3bd483a69f6104282376e938cde75a00e281177
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-10-12 12:19:46 +0530

    Time: 108 ms (21.32%), Space: 62.5 MB (19.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,27 +3,28 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let rows = grid.length, columns = grid[0].length;
-    let set = new Set();
+    let rows = grid.length, columns = grid[0].length
+    let set = new Set()
     
     function dfs(r,c){
-        if(r < 0 || r >= rows || c < 0 || c >= columns || grid[r][c] == 0 || set.has((r + "," + c))){
-            return 0;
-        }
+        if(r < 0 || c < 0 || r >= rows || c >= columns || grid[r][c] === "0" || set.has((r + "," + c))) return 0;
         
-        set.add((r + "," + c));
-        
-        return (1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1));
+        set.add((r + "," + c))
+        dfs(r+1, c)
+        dfs(r-1, c)
+        dfs(r, c+1)
+        dfs(r, c-1)
+        return 1;
     }
     
     let count = 0;
     for(let r = 0; r < rows; r++){
         for(let c = 0; c < columns; c++){
             if(dfs(r,c)){
-                count++;
+                count++
             }
         }
     }
-    return count;
     
+    return count;
 };
