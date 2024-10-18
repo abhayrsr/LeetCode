2506c4062d24ab4dac27726cce99062590cfcbf2
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-10-18 11:29:11 +0530

    Time: 9 ms (100.00%), Space: 55.6 MB (86.67%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.js
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.js
@@ -5,25 +5,23 @@
 var countCompleteSubarrays = function(nums) {
     let set = new Set(nums);
     let left = 0, right = 0, count = 0;
-    let map = new Map();
+    let map = new Map()
     
-    
+    while(right < nums.length){
+        map.set(nums[right], (map.get(nums[right]) || 0) + 1)
         
-        for(let right = 0; right < nums.length; right++){
-            map.set(nums[right], (map.get(nums[right]) || 0)   + 1)
-            
-            while(set.size === map.size){
-                count += nums.length - right
-                
-                map.set(nums[left], map.get(nums[left])-1)
-                
-                if(map.get(nums[left]) === 0){
-                    map.delete(nums[left])
-                }
-                left++;
+        while(set.size === map.size){
+            count += nums.length - right
+            map.set(nums[left], map.get(nums[left]) - 1)
+            if(map.get(nums[left]) === 0){
+                map.delete(nums[left])
             }
+            left++;
         }
-       
+        
+        right++;
+    }
     
-    return count
+    return count;
+    
 };
