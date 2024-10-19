ba6562aa6aa4f031f21592b0678614839bffe18c
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-10-19 11:58:32 +0530

    Time: 14 ms (100.00%), Space: 50.8 MB (87.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -1,36 +1,47 @@
 var checkInclusion = function (s1, s2) {
-  const map = new Map();
-
-  for (let char of s1) {
-    map.set(char, map.get(char) + 1 || 1);
-  }
-
-  let left = 0;
-  let right = 0;
-
-  while (left <= right && right < s2.length) {
-      const charRight = s2.charAt(right)
-      if(map.get(charRight) > 0){
-          map.set(charRight, map.get(charRight) - 1)
-          right++
-      } else if(!map.has(charRight)){
-           while(left != right){
-              const charLeft = s2[left++];
-              if (map.has(charLeft)) map.set(charLeft, map.get(charLeft) + 1);
-              }
-          
-              left++;
-              right++;
-          
-      }  else {
-          const charLeft = s2.charAt(left)
-          
-          map.set(charLeft, map.get(charLeft) + 1)
-          left++
-      }
-      
-      if (right - left >= s1.length) return true;
-      
-  }
-  return false;
+    
+    if(s1.length > s2.length) return false;
+    let s1Count = Array(26).fill(0)
+    let s2Count = Array(26).fill(0)
+    
+    
+    for(let i = 0;  i < s1.length; i++){
+        s1Count[s1.charCodeAt(i) - 'a'.charCodeAt(0)] += 1
+        s2Count[s2.charCodeAt(i) - 'a'.charCodeAt(0)] += 1
+    }
+    
+    
+    let matches = 0
+    
+    for(let i = 0; i < 26; i++){
+        if(s1Count[i] === s2Count[i]) matches += 1
+    }
+    
+    let left = 0
+    
+    for(let right = s1.length; right < s2.length; right++){
+        if(matches === 26) return true;
+        
+        let index = s2.charCodeAt(right) - 'a'.charCodeAt(0)
+        s2Count[index] += 1;
+        
+        if(s1Count[index] === s2Count[index]){
+            matches += 1
+        } else if(s1Count[index] + 1 === s2Count[index]){
+            matches -= 1
+        }
+        
+        index = s2.charCodeAt(left) - 'a'.charCodeAt(0)
+        s2Count[index] -= 1
+        
+        if(s1Count[index] === s2Count[index]){
+            matches += 1
+        } else if(s1Count[index] - 1 === s2Count[index]){
+            matches -= 1
+        }
+        
+        left++;
+    }
+    
+    return matches === 26
 };
