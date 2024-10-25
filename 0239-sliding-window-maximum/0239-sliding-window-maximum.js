94101fa07f4455f511c4a7adf5692ccf8eeb1378
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-10-25 23:13:12 +0530

    Time: 436 ms (67.44%), Space: 82.1 MB (40.86%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.js
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -26,21 +26,3 @@ var maxSlidingWindow = function(nums, k) {
     return output;
 };
 
-//  const res = [];
-//     const queue = [];
-//     let [l, r] = [0, 0];
-
-//     while (r < nums.length) {
-//         while (queue.length && nums[queue[queue.length - 1]] < nums[r]) {
-//             queue.pop();
-//         }
-//         queue.push(r);
-//         if (l > queue[0]) queue.shift();
-
-//         if (r + 1 >= k) {
-//             res.push(nums[queue[0]]);
-//             l += 1;
-//         }
-//         r += 1;
-//     }
-//     return res;
