9f463dc51a3b0f98a3b76fe262467e8e3b58b193
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-10-28 11:13:25 +0530

    Time: 2 ms (35.66%), Space: 50.5 MB (57.24%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let stack = []
+    let result = []
+    
+    function backtrack(openN, closedN){
+        if(openN === n && closedN === n){
+            result.push(stack.join(''))
+            return
+        }
+        
+        if(openN < n){
+            stack.push("(")
+            backtrack(openN + 1, closedN)
+            stack.pop()
+        }
+        
+        if(closedN < openN){
+            stack.push(")")
+            backtrack(openN, closedN + 1)
+            stack.pop()
+        }
+    }
+    backtrack(0, 0)
+    return result;
+};
