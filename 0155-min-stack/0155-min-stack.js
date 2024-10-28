fcefbba5b83da3f16e5943b526024c3c2c8523f5
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-10-28 10:06:45 +0530

    Time: 18 ms (29.14%), Space: 59.7 MB (27.23%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -1,6 +1,7 @@
 
 var MinStack = function() {
-    this.st = [];
+    this.stack = []
+    this.minStack = []
 };
 
 /** 
@@ -8,30 +9,42 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(val) {
-    let min_val = this.getMin();
-    if (min_val === null || min_val > val) {
-      min_val = val;
+    this.stack.push(val)
+    if(this.minStack.length === 0){
+       return this.minStack.push(val)
+    } else {
+        val = Math.min(val, this.minStack[this.minStack.length - 1])
+       return this.minStack.push(val)
     }
-    this.st.push([val, min_val]);
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-    this.st.pop();
+    this.stack.pop()
+    this.minStack.pop()
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    return this.st.length ? this.st[this.st.length - 1][0] : null;
+    return this.stack[this.stack.length - 1]
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    return this.st.length ? this.st[this.st.length - 1][1] : null;
+    return this.minStack[this.minStack.length - 1]
 };
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
