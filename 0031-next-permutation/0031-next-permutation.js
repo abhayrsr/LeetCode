613e8d5dac16dbe488b959c0555b165cb819c772
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-12-11 13:05:32 +0530

    Time: 0 ms (100.00%), Space: 52.2 MB (12.10%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -3,34 +3,30 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-    let index = -1;
+    let index = -1
     
-    for(let i = nums.length-2; i >= 0; i--){
+    for(let i = nums.length - 2; i >= 0; i--){
         if(nums[i] < nums[i+1]){
-            index = i;
+            index = i
             break;
         }
     }
-    
-    if(index == -1) return nums.reverse();
     
     for(let i = nums.length-1; i >= index; i--){
         if(nums[i] > nums[index]){
-            [nums[i], nums[index]] = [nums[index], nums[i]];
+            [nums[i], nums[index]] = [nums[index], nums[i]]
             break;
         }
     }
     
+    let i = nums.length - 1, j = index+1
     
-    function reverse(arr, startIndex, endIndex) {
-    while (startIndex < endIndex) {
-        [arr[startIndex], arr[endIndex]] = [arr[endIndex], arr[startIndex]];
-        startIndex++;
-        endIndex--;
+    while(j < i){
+        [nums[i], nums[j]] = [nums[j], nums[i]]
+        i--;
+        j++;
     }
-        return arr;
-}
     
-    return reverse(nums, index + 1, nums.length - 1);
+    return nums
     
 };
