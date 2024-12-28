21a72bdea36bf09715a8e9a5e903be26b717aa40
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2024-12-28 22:36:32 +0530

    Time: 19 ms (80.03%), Space: 74 MB (37.02%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,15 +3,17 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-     let ans = new Array(temperatures.length).fill(0)
-     let stack = []
-     
-     for(let i = 0; i < temperatures.length; i++){
-         while(stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]){
-             let prevDay = stack.pop()
-             ans[prevDay] = i - prevDay
-         }
-         stack.push(i)
-     }
-    return ans;
+    let stack = []
+    let result = new Array(temperatures.length).fill(0)
+    
+    for(let i = 0; i < temperatures.length; i++){
+
+        while(stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]){
+            let prev = stack.pop()
+            result[prev] = i - prev
+        }
+        stack.push(i)
+    }
+    
+    return result
 };
