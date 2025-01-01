3008be10c0fc72c2f3d071754227169786bf9064
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2025-01-01 11:02:07 +0530

    Time: 2 ms (62.44%), Space: 49 MB (89.85%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.js
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.js
@@ -4,22 +4,23 @@
  */
 var maxScore = function(s) {
     let sumZero = 0, sumOne = 0, max = -Infinity;
-    
+
     for(let i = 0; i < s.length; i++){
-        if(s[i] == '1'){
-            sumOne++
+        if(s[i] === '1'){
+            sumOne++;
         }
     }
-    console.log(sumOne)
-    
+
     for(let i = 0; i < s.length - 1; i++){
-        if(s[i] == '0'){
-            sumZero++;
+        if(s[i] === '0'){
+            sumZero++
         }
-        if(s[i] == '1'){
-            sumOne--;
+
+        if(s[i] === '1'){
+            sumOne--
         }
-        max = Math.max(max, sumOne+sumZero)
+        
+        max = Math.max(max, sumOne + sumZero)
     }
     return max;
 };
