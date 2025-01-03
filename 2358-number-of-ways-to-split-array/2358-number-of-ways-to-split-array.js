f594aadc784322ae1f74001c097106aabf74bf70
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2025-01-03 22:23:02 +0530

    Time: 10 ms (88.89%), Space: 61.6 MB (91.11%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.js
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var waysToSplitArray = function(nums) {
-    let space = [], sum1 = 0
+    let sum1 = 0
     for(let i = 0; i < nums.length; i++ ){
         sum1 += nums[i]
-        space.push(sum1)
     }
-    
-    let count = 0, sum = 0
 
-    for(let i = nums.length - 1; i >= 1; i--){
+    let count = 0, sum = 0
+    for(let i = 0; i < nums.length - 1; i++){
         sum += nums[i]
-        if(space[i-1] >= sum){
+        sum1 -= nums[i]
+
+        if(sum >= sum1){
             count++
         }
     }
