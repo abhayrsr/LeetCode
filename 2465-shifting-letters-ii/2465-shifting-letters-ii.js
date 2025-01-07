9d9edb91b03f70d10612f29f20e2ee824df57302
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2025-01-07 20:53:39 +0530

    Time: 10 ms (99.85%), Space: 78.3 MB (11.78%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.js
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.js
@@ -1,24 +1,19 @@
 var shiftingLetters = function(s, shifts) {
-    const n = s.length;
-    const shift = Array(n + 1).fill(0);
+    let diff = Array(s.length + 1).fill(0);
 
-    for (let shiftOp of shifts) {
-        const [start, end, direction] = shiftOp;
-        shift[start] += direction === 1 ? 1 : -1;
-        if (end + 1 < n) shift[end + 1] -= direction === 1 ? 1 : -1;
+    for(let i = 0; i < shifts.length; i++){
+        diff[shifts[i][0]] += shifts[i][2] === 1 ? 1 : -1;
+        diff[shifts[i][1] + 1] -= shifts[i][2] === 1 ? 1 : -1;
     }
 
     let currentShift = 0;
-    let result = s.split('');
-    for (let i = 0; i < n; ++i) {
-        currentShift += shift[i];
-        shift[i] = currentShift;
+    let result = [];
+
+    for(let i = 0; i < s.length; i++){
+        currentShift += diff[i];
+        let character = (s.charCodeAt(i) - 'a'.charCodeAt(0) + currentShift) % 26
+        result.push(String.fromCharCode((character + 26) % 26 + 'a'.charCodeAt(0)))
     }
 
-    for (let i = 0; i < n; ++i) {
-        let netShift = (shift[i] % 26 + 26) % 26;
-        result[i] = String.fromCharCode('a'.charCodeAt(0) + (s.charCodeAt(i) - 'a'.charCodeAt(0) + netShift) % 26);
-    }
-
-    return result.join('');
+    return result.join("")    
 };
