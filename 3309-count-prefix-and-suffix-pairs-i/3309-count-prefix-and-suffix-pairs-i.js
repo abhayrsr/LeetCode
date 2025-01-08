83d24612a4c2015270a7b3313d896b6088982c36
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2025-01-08 22:58:17 +0530

    Time: 263 ms (5.32%), Space: 58 MB (5.32%) - LeetHub

---
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var countPrefixSuffixPairs = function(words) {
+    let count = 0;
+    let pre = '', suff = '';
+
+    for(let i = 0; i < words.length; i++){
+        let length = words[i].length;
+        for(let j = i+1; j < words.length; j++){
+           
+             for(let k = 0; k < length; k++){
+                pre += words[j][k]
+            }
+
+            for(let k = words[j].length - length; k < words[j].length; k++){
+                suff += words[j][k]
+            }
+
+            console.log(words[i], pre, suff)
+
+            if((words[i] === pre) && (pre === suff)){
+                count++
+            }
+            
+           
+           pre = ''
+           suff = ''
+        }
+    }
+
+    return count;
+};
