182798a76baebac755994970a72f92cb4b640cb8
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2025-02-04 00:08:33 +0530

    Time: 27 ms (8.35%), Space: 62.7 MB (5.01%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    let dummyNode = new ListNode(-1);
+    dummyNode.next = head;
+    let slow = dummyNode, fast = dummyNode;
+
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    let start = slow.next, curr = start, prev = null;
+    slow.next = null;
+
+    while(curr){
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+
+    console.log(head, prev);
+
+    let l1 = head, l2 = prev, final = head;
+
+    while(l1 && l2){
+        let temp1 = l1.next;
+        l1.next = l2;
+        let temp2 = l2.next;
+        l1 = temp1;
+        l2.next = temp1;
+        l2 = temp2; 
+    }
+    console.log(head)
+};
