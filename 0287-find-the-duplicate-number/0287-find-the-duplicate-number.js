fc8cf535326a8eeecae71cff5862b2e53fd09436
Abhay Raj <30930553+abhayrsr@users.noreply.github.com>
2025-02-04 05:03:15 +0530

    Time: 6 ms (86.05%), Space: 57.6 MB (85.14%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -3,18 +3,23 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    let slow = nums[0]
-    let fast = nums[0]
-    
-    do{
-        slow = nums[slow]
-        fast = nums[nums[fast]]
-    } while(slow != fast)
-    
-    fast = nums[0]
-    while(slow != fast){
-        slow = nums[slow]
-        fast = nums[fast]
+    let slow = 0, fast = 0;
+   
+    while(true){
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if(slow === fast){
+            break;
+        }
     }
-    return slow;
+
+    let slowNew = 0;
+
+    while(true){
+        slowNew = nums[slowNew];
+        slow = nums[slow];
+        if(slowNew === slow){
+            return slowNew;
+        }
+    }
 };
